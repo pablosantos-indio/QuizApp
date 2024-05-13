dfd3660ef52efbf76de90a366257f405621a7089
SantosPablo <pabloasantos@gmail.com>
2024-05-13 18:39:36 -0300

    correct error

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -68,7 +68,7 @@ export default function Home(props) {
   const renderContent = () => {
     if (selectedItem === 'Professor') {
       return (
-        <ResponsiveStepper />
+        <ResponsiveStepper selectedItem={selectedItem} />
       );
     } else if (selectedItem === 'Student') {
       return (
